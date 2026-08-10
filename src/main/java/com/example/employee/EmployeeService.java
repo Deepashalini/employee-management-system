8bb340fa31d2ed029a607b381261e3f624f837d3
Ubuntu <ubuntu@ip-172-31-9-134.ap-southeast-2.compute.internal>
2026-08-10 12:12:55 +0000

    Developer A adds employee API

---
+++ src/main/java/com/example/employee/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.employee;
+
+public class EmployeeService {
+
+      public String getEmployee() {
+             return "Employee API added by Developer A";
+      }
+}
---
+++ src/test/java/com/example/employee/EmployeeServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.employee;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EmployeeServiceTest {
+
+    @Test
+    void testGetEmployee() {
+        EmployeeService service = new EmployeeService();
+
+        assertEquals(
+            "Employee API added by Developer A",
+            service.getEmployee()
+        );
+    }
+}
